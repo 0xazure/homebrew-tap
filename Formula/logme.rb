048643251ca6e17eee8e9589af912bedc0871c2e
0xazure <stuart.crust@gmail.com>
2019-06-30 21:33:23 -0400

    add logme

---
+++ Formula/logme.rb
@@ -0,0 +1,15 @@
+class Logme < Formula
+  desc "XDG Base Directory Specification-compliant command-line note-taking program"
+  homepage "https://github.com/0xazure/logme"
+  url "https://github.com/0xazure/logme/releases/download/v0.1.0/logme-v0.1.0-x86_64-apple-darwin.tar.gz"
+  version "0.1.0"
+  sha256 "61ddb627220f16871fefa85051f00e7f3d99749c8cc2bb53cec1e42edbe1748c"
+
+  def install
+    bin.install "logme"
+  end
+
+  test do
+    system "#{bin}/logme"
+  end
+end
