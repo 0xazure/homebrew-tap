68eb13bdf7ec292ab3ada4ee759681a31dd74e4a
0xazure <stuart.crust@gmail.com>
2019-07-03 21:37:50 -0400

    update logme to v0.1.1

--- Formula/logme.rb
+++ Formula/logme.rb
@@ -1,8 +1,8 @@
 class Logme < Formula
   desc "XDG Base Directory Specification-compliant command-line note-taking program"
   homepage "https://github.com/0xazure/logme"
-  url "https://github.com/0xazure/logme/releases/download/v0.1.0/logme-v0.1.0-x86_64-apple-darwin.tar.gz"
-  version "0.1.0"
+  url "https://github.com/0xazure/logme/releases/download/v0.1.1/logme-v0.1.1-x86-64-apple-darwin.tar.gz"
+  version "0.1.1"
   sha256 "61ddb627220f16871fefa85051f00e7f3d99749c8cc2bb53cec1e42edbe1748c"
 
   def install
